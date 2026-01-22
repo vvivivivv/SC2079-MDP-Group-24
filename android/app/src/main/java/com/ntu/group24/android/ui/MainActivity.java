774f65dc6a763628173a11293a6550a497f77b96
xy <xxinyitoh@gmail.com>
2026-01-22 21:15:46 +0800

    chore(android): implement runtime permissions and define constants

--- android/app/src/main/java/com/ntu/group24/android/ui/MainActivity.java
+++ android/app/src/main/java/com/ntu/group24/android/ui/MainActivity.java
@@ -6,13 +6,24 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.ntu.group24.android.R;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSIONS = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Req for bluetooth and location permissions
+        checkPermissions();
 
         // Initialise UI components and set adapter
         TabLayout tabLayout = findViewById(R.id.tab_layout);
@@ -28,5 +39,33 @@ public class MainActivity extends AppCompatActivity {
                 case 2: tab.setText("BLUETOOTH"); break;
             }
         }).attach();
+    }
+    private void checkPermissions() {
+        Log.d("MainActivity", "Starting permission check...");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_CONNECT,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    }, REQUEST_CODE_PERMISSIONS);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permissions Denied! Bluetooth will not work.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
